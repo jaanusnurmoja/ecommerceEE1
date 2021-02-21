b35af083435df1a68c59f3d68cdeac7c20e5fc44
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2021-02-21 14:31:40 +0200

    price to product view + user

---
+++ src/main/java/ee/sda/ecommerce/entities/UserEE1.java
@@ -0,0 +1,46 @@
+package ee.sda.ecommerce.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Collection;
+
+@Getter
+@Setter
+@Entity
+public class UserEE1 implements UserDetails {
+    @Id
+    private Long id;
+    private String username;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
